000031afb7ec0535da359334caf67caadd7682f0
isaac <casvel320@gmail.com>
2023-08-18 15:30:22 -0600

    cambios generales

--- gic_graph.py
+++ gic_graph.py
@@ -48,15 +48,22 @@ idate = sys.argv[1]# "formato(yyyymmdd)"
 fdate = sys.argv[2]
 
 #idate = input("write initial date in format yyyy-mm-dd \n >  " )
-#fdate = input("write final date in format yyyy-mm-dd \n >  " )
-
-
-
-    
+#fdate = input("write final date in format yyyy-mm-dd \n >  " )    
 gicTW_lav = (df_lav['LAV'].gic_proc[idate:fdate])
 gicTW_qro = (df_qro['QRO'].gic_proc[idate:fdate])
 gicTW_rmy = (df_rmy['RMY'].gic_proc[idate:fdate])
 gicTW_mzt = (df_mzt['MZT'].gic_proc[idate:fdate])
+
+
+T1TW_lav = (df_lav['LAV'].T1_proc[idate:fdate])
+T1TW_qro = (df_qro['QRO'].T1_proc[idate:fdate])
+T1TW_rmy = (df_rmy['RMY'].T1_proc[idate:fdate])
+T1TW_mzt = (df_mzt['MZT'].T1_proc[idate:fdate])
+
+T2TW_lav = (df_lav['LAV'].T2_proc[idate:fdate])
+T2TW_qro = (df_qro['QRO'].T2_proc[idate:fdate])
+T2TW_rmy = (df_rmy['RMY'].T2_proc[idate:fdate])
+T2TW_mzt = (df_mzt['MZT'].T2_proc[idate:fdate])
 ###############################################################################
 ###############################################################################
 dir_path = '/home/isaac/MEGAsync/datos/dH_coe/'
@@ -83,7 +90,9 @@ for value in k:
 
 inicio = gicTW_lav.index[0]
 final  = gicTW_lav.index[-1]
-
+##############################################################################################
+#fig 1
+##############################################################################################
 fig, ax = plt.subplots(6, figsize=(12,14))
 fig.suptitle('Estudio de GICs, 2023', fontsize=24, fontweight='bold')
 
@@ -130,8 +139,64 @@ fig.tight_layout()
 fig.savefig("/home/isaac/geomstorm/rutpy/gicsOutput/gic_obs_"+\
             str(inicio)[0:10]+"_"+str(final)[0:10]+".png")
 plt.show()
+##############################################################################################
+#fig 2
+##############################################################################################
+fig, ax = plt.subplots(6, figsize=(12,14))
+fig.suptitle('Estudio de GICs, 2023', fontsize=24, fontweight='bold')
 
+ax[0].plot(T1TW_lav, label='T1')
+ax[0].plot(T2TW_lav, label='T2')
+ax[0].grid()
+ax[0].set_xlim(inicio,final)
+ax[0].set_title('LAV st', fontsize=18)
+ax[0].set_ylabel(' Temperatura [C°]', fontweight='bold')
+ax[0].legend()
+
+ax[1].plot(T1TW_qro, label='T1')
+ax[1].plot(T2TW_qro, label='T2')
+ax[1].grid()
+ax[1].set_xlim(inicio,final)
+ax[1].set_title('QRO st', fontsize=18)
+ax[1].set_ylabel(' Temperatura [C°]', fontweight='bold')
+ax[1].legend()
+
+ax[2].plot(T1TW_rmy, label='T1')
+ax[2].plot(T2TW_rmy, label='T2')
+ax[2].grid()
+ax[2].set_xlim(inicio,final)
+ax[2].set_title('RMY st', fontsize=18)
+ax[2].set_ylabel(' Temperatura [C°]', fontweight='bold')
+ax[2].legend()
+
+ax[3].plot(T1TW_mzt, label='T1')
+ax[3].plot(T2TW_mzt, label='T2')
+ax[3].grid()
+ax[3].set_xlim(inicio,final)
+ax[3].set_title('MZT st', fontsize=18)
+ax[3].set_ylabel(' Temperatura [C°]', fontweight='bold')
+ax[3].legend()
+
+ax[4].plot(H, color='k')
+ax[4].set_ylabel(' DH [nT]', fontweight='bold')
+ax[4].set_title('Indices geomagnéticos, Estación Coeneo', fontsize=18)
+ax[4].grid()
+ax[4].set_xlim(inicio,final)
+
+
+ax[5].bar(k.index, k, width = 0.1, align='edge', color=colorsValue)
+ax[5].set_ylim(0,9)
+ax[5].set_xlim(inicio,final)
+ax[5].set_ylabel(' Kcoe', fontweight='bold')
+ax[5].grid()
+
+fig.tight_layout()
+
+fig.savefig("/home/isaac/geomstorm/rutpy/gicsOutput/T_obs_"+\
+            str(inicio)[0:10]+"_"+str(final)[0:10]+".png")
+plt.show()
 end = timer()
+
 print(end - start)
 '''
 
